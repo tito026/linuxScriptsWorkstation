3a5b6b53465a40f27759c66b1b31c9b3aa73a5e3
Tito Luisjuan <tito.guerrero026@gmail.com>
2019-10-30 08:02:33 -0600

    fix

--- udgnet.sh
+++ udgnet.sh
@@ -1,1 +1,1 @@
-rdesktop -z -N -r clipboard:PRIMARYCLIPBOARD -k en-us -g 1425x840 -d udgnet -u safsa -p sasfa 10.104.1.25
+rdesktop -z -N -r clipboard:PRIMARYCLIPBOARD -k en-us -g 1425x840 -d u -u safsa -p sasfa 10.104.1.25
